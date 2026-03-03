625ddae708a3e7fedeb6ecd1ad7040e1015db228
NovaCript <soltivco@gmail.com>
2026-03-03 20:02:58 +0300

    fake data generator

--- Api/Storage/ContactStorage.cs
+++ Api/Storage/ContactStorage.cs
@@ -4,6 +4,16 @@ public class ContactStorage
     public ContactStorage()
     {
         this.Contacts = new List<Contact>();
+
+        for (int i = 1; i <= 5; i++)
+        {
+            this.Contacts.Add(new Contact()
+            {
+                Id = i,
+                Name = $"Full name {i}",
+                Email = $"{Guid.NewGuid().ToString().Substring(0, 5)}_{i}@gmail.com"
+            });
+        }
     }
 
     public List<Contact> Contacts { get; set; }
