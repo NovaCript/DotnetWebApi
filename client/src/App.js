d223a41bd5be678102550dda564c611be76a3ee4
NovaCript <soltivco@gmail.com>
2026-03-04 15:44:57 +0300

    16.8 Добавление элемента в набор данных

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,17 @@ const contacts = [
   { id: 1, name: "Имя Фамилия 1", email: "q1@e.rt" },
   { id: 2, name: "Имя Фамилия 2", email: "q2@e.rt" },
   { id: 3, name: "Имя Фамилия 3", email: "q3@e.rt" },
-  { id: 3, name: "Имя Фамилия 3", email: "q3@e.rt" },
-  { id: 3, name: "Имя Фамилия 3", email: "q3@e.rt" },
-  { id: 3, name: "Имя Фамилия 3", email: "q3@e.rt" },
-  { id: 3, name: "Имя Фамилия 3", email: "q3@e.rt" },
-  { id: 3, name: "Имя Фамилия 3", email: "q3@e.rt" },
-  { id: 3, name: "Имя Фамилия 3", email: "q3@e.rt" },
-  { id: 3, name: "Имя Фамилия 3", email: "q3@e.rt" },
-  { id: 3, name: "Имя Фамилия 3", email: "q3@e.rt" },
-  { id: 3, name: "Имя Фамилия 3", email: "q3@e.rt" },
 ];
+
+const addContact = () => {
+  const item = {
+    id: Math.floor(Math.random() * 100),
+    name: "Имя Фамилия 3",
+    email: "q3@e.rt",
+  };
+  contacts.push(item);
+  console.log(contacts);
+};
 
 const App = () => {
   return (
@@ -24,6 +25,16 @@ const App = () => {
         </div>
         <div className="card-body">
           <TableContact contacts={contacts} />
+          <div>
+            <button
+              className="btn btn-primary"
+              onClick={() => {
+                addContact();
+              }}
+            >
+              Добавить контакт
+            </button>
+          </div>
         </div>
       </div>
     </div>
