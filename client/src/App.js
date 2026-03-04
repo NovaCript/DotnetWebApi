55a8070968f147365324dbb87a7c8aecb1972d92
NovaCript <soltivco@gmail.com>
2026-03-04 14:00:44 +0300

    16.3 Подготовка представления

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,41 @@
-import "./App.css";
-
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <div>
+        <div>
+          <h1>Список контактов</h1>
+        </div>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Имя контакта</th>
+                <th>E-mail</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <tb>1</tb>
+                <tb>Name 1</tb>
+                <tb>content1@email.com</tb>
+              </tr>
+              <tr>
+                <tb>2</tb>
+                <tb>Name 2</tb>
+                <tb>content2@email.com</tb>
+              </tr>
+              <tr>
+                <tb>3</tb>
+                <tb>Name 3</tb>
+                <tb>content3@email.com</tb>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
