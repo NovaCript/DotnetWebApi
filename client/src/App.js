3f0a5ee6d6c12746ebd73d7827cb14b7a6fe6b3f
NovaCript <soltivco@gmail.com>
2026-03-04 15:49:14 +0300

    16.9 React-хуки в деле

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,20 @@
 import TableContact from "./layout/TableContact/TableContact";
-
-const contacts = [
-  { id: 1, name: "Имя Фамилия 1", email: "q1@e.rt" },
-  { id: 2, name: "Имя Фамилия 2", email: "q2@e.rt" },
-  { id: 3, name: "Имя Фамилия 3", email: "q3@e.rt" },
-];
-
-const addContact = () => {
-  const item = {
-    id: Math.floor(Math.random() * 100),
-    name: "Имя Фамилия 3",
-    email: "q3@e.rt",
-  };
-  contacts.push(item);
-  console.log(contacts);
-};
-
+import React, { useState } from "react";
 const App = () => {
+  const [contacts, setContacts] = useState([
+    { id: 1, name: "Имя Фамилия 1", email: "q1@e.rt" },
+    { id: 2, name: "Имя Фамилия 2", email: "q2@e.rt" },
+    { id: 3, name: "Имя Фамилия 3", email: "q3@e.rt" },
+  ]);
+  const addContact = () => {
+    const item = {
+      id: Math.floor(Math.random() * 100),
+      name: "Имя Фамилия 3",
+      email: "q3@e.rt",
+    };
+    setContacts([...contacts, item]);
+  };
+
   return (
     <div className="container mt-5">
       <div className="card">
