327aa4f42b83c55e07f8e6d27900232cc31adc8c
NovaCript <soltivco@gmail.com>
2026-03-15 19:05:23 +0300

    extension add

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,24 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
-var stringConnection = builder.Configuration.GetConnectionString("SqliteStringConnection");
-
-
-builder.Services.AddControllers();
-builder.Services.AddSwaggerGen(opt =>
-{
-    opt.SwaggerDoc("v1", new Microsoft.OpenApi.OpenApiInfo
-    {
-        Title = "API списка контактов",
-    });
-});
-
-builder.Services.AddCors(opt =>
-opt.AddPolicy("CorsPolicy", policy =>
-{
-    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins(builder.Configuration["client"]);
-}));
-
-
-builder.Services.AddSingleton<IStorage>(new SqliteStorage(stringConnection));
+builder.Services.AddServiceCollection(builder.Configuration);
 
 var app = builder.Build();
 
@@ -29,6 +10,5 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapControllers();
-
 app.UseCors("CorsPolicy");
 app.Run();
