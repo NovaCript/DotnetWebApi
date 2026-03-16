c6ef024521181bdfe83ec52e26460aae1ecf6026
NovaCript <soltivco@gmail.com>
2026-03-16 16:43:23 +0300

    create seed fake data

---
+++ Api/Extensions/ApplicationServiceProvider.cs
@@ -0,0 +1,22 @@
+using System.Windows.Markup;
+
+public static class ApplicationServiceProvider
+{
+    public static IServiceProvider AddCustomService(
+        this IServiceProvider services,
+        IConfiguration configuration
+    )
+    {
+        using var scope = services.CreateScope();
+
+        var storage = scope.ServiceProvider.GetService<IStorage>();
+        var dbStorage = storage as SqliteStorage;
+        if (dbStorage != null)
+        {
+            string cs = configuration.GetConnectionString("SqliteStringConnection");
+            new FakerInitializer(cs).Initialize();
+        }
+
+        return services;
+    }
+}
